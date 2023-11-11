74ae37f57b52ccd3be56b41006a251650f38b912
Wyatt Weller <wyattzyatt@gmail.com>
2023-11-10 18:07:12 -0700

    (TS) Work: Worked on a method of recording times

--- Subcom15_DummyDelay.m
+++ Subcom15_DummyDelay.m
@@ -1,7 +1,27 @@
+function [Output, DelayedTime] =  Subcom15_DummyDelay(Input, DelayTime)
 % --------------------------
 % 15Subcom Capstone Project
 % Dummy Delay
 % Wyatt Weller
 % 11/10/2023
 % --------------------------
+% This Script will be imitating the latency of our communication, and will
+% be treated as if it is the function being called and return the variables
+% as if it was an immitation of our physical system sending and receiving
+% commands and thus it will be taking a command as an input, and returning
+% the same command as a return variable
+% 
+% Use Case:
+% 
+% Input: The input binary array given from the Test System
+% Output: The output binary array returned to the Test System
+% DelayTime: The ammount of delay to create (-1 for random) in miliseconds
+% 
+% Output = DummyDelay(Input, DelayTime)
+% 
 % --------------------------
+
+Timer = tic();
+pause(DelayTime/1000);
+DelayedTime = toc(Timer);
+Output = Input;
--- Subcom15_TestSystem.m
+++ Subcom15_TestSystem.m
@@ -5,3 +5,42 @@
 % 11/10/2023
 % --------------------------
 % --------------------------
+close all; clear all; clc;
+
+% Testing Parameters
+MaxTest = 1000;
+MinTest = 500;
+TestInterval = 15;
+TestRange = MinTest:TestInterval:MaxTest;
+
+% Array Memory Pre-Allocation
+Error = zeros(size(TestRange));
+AvgError = zeros(size(TestRange));
+Output = zeros(size(TestRange));
+TestTimes = zeros(size(TestRange));
+TotalTimes = zeros(size(TestRange));
+j=1;
+
+% Testing/Timing
+for i = TestRange
+    Timer1 = tic();
+    [Output(j),TestTimes(j)] = Subcom15_DummyDelay("10101010",i);
+    TotalTimes(j) = toc(Timer1);
+    Error(j) = TotalTimes(j)-TestTimes(j);
+    AvgError(j) =  sum(Error)/length(find(Error~=0));
+    j=j+1;
+end
+
+% Plotting
+figure(1);
+subplot(211);
+plot(TestRange,Error*1000);hold on;
+semilogy(TestRange,AvgError*1000);hold off;
+xlabel("Testing Time (ms)")
+ylabel("Resulting Error (ms)")
+subplot(212);
+plot(TestRange,TotalTimes*1000);hold on;
+semilogy(TestRange,TestTimes*1000);hold off;
+xlabel("Testing Time (ms)")
+ylabel("Resulting Time (ms)")
+
