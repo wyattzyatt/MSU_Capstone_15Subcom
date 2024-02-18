e9b8fcadc0f3829f9388890252183f317583d060
KnightS32 <80178197+KnightS32@users.noreply.github.com>
2024-02-17 19:04:16 -0700

    Add demodulation function

---
+++ demod.m
@@ -0,0 +1,21 @@
+%% BFSK Demodulation Function
+function demod_sig = demod(bfsk, F1, F2, fs, bitLength)
+
+    % Compute the analytical signal (complex envelope)
+    analytic_signal = hilbert(bfsk);
+
+    % Compute the instantaneous phase of the analytic signal
+    instantaneous_phase = unwrap(angle(analytic_signal));
+
+    % Compute the instantaneous frequency (derivative of phase)
+    instantaneous_frequency = diff(instantaneous_phase) / (2 * pi * (1/fs));
+
+    % Set a threshold to decide between the two frequencies
+    threshold = (F1 + F2) / 2;
+
+    % Demodulation based on instantaneous frequency
+    demod_sig = zeros(size(bfsk));
+    demod_sig(instantaneous_frequency >= threshold) = 1;
+
+    demod_sig = demod_sig(round((fs/bitLength)/2):round(fs/bitLength):end);
+end
