a75198e872eb176c71c664ca4310f74746047dd8
Wyatt Weller <wyattzyatt@gmail.com>
2024-02-29 10:21:49 -0700

    Fixed testing for removing commands

--- Subcom15_PsudoSub.py
+++ Subcom15_PsudoSub.py
@@ -9,19 +9,20 @@ Created on Mon Feb 5 2024
 from Subcom15_Communicator import Communicator
 import random
 import time
+from pytictoc import TicToc
 
 def testCommunicators(numCommunicators):
     totalCommands = list( None for _ in range(numCommunicators*256))
     for comNum in range(numCommunicators):
         communicator = Communicator(f"Test Communicator {comNum}",f"Command0")
-        numCommands = 1
         while 1:
             # Generate a random number of commands to add and remove
             addCount = random.randint(5, 10)
             removeCount = random.randint(1,5)
+            currCount = len(communicator.commandList())
 
-            if numCommands > 255:
-                print(f"Communicator {comNum} reached maximum commands: {numCommands}")
+            if currCount > 255:
+                print(f"Communicator {comNum} reached maximum commands: {len(communicator.commandList())}")
                 # Send all commands in random order
                 shuffledCommands = random.sample(communicator.commandList(), len(communicator.commandList()))
                 commandNum = 0
@@ -38,30 +39,27 @@ def testCommunicators(numCommunicators):
                 break
             else:
                 # Add commands
-                for i in range(addCount):
-                    if numCommands <= 255:
-                        communicator.addCommand(f"Command{numCommands}")
-                        numCommands += 1
+                while len(communicator.commandList()) < currCount + addCount:
+                    if len(communicator.commandList()) <= 255:
+                        communicator.addCommand(f"Command{random.randint(0, 1024)}")
+                    else:
+                        break
                 
                 # Remove commands from random locations
-                # removeCommands = random.sample(communicator.commandList(), removeCount)
-                # for cmd in removeCommands:
-                #     if numCommands < 255:
-                #         communicator.removeCommand(cmd)
-                #         numCommands -= 1
+                removeCommands = random.sample(communicator.commandList(), removeCount)
+                for cmd in removeCommands:
+                    if len(communicator.commandList()) < 255:
+                        communicator.removeCommand(cmd)
     return totalCommands
                 
             
 # Testing
-totalCommands = testCommunicators(10)
+totalCommands = testCommunicators(1)
+print(f"Total number of commands: {len(totalCommands)}")
 print(f"Sent | Received")
 print(totalCommands)
 # communicator = Communicator(f"Test Communicator {0}",f"Command0")
 
-# numCommands = 1
-# while numCommands <= 255:
-#     communicator.addCommand(f"Command{numCommands}")
-#     numCommands += 1
 
 # random_command = random.choice(communicator.commandList())
 # communicator.setSendCommand(random_command)
