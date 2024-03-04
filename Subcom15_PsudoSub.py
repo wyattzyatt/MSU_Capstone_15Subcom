80d4855e0fb3af1bc617586c9b91aa1deb24755e
Wyatt Weller <wyattzyatt@gmail.com>
2024-03-04 11:01:59 -0700

    Reworked PsudoSub to test as planned for Req 1.1 Veri Test
    It now has the ability to send or receive commands first or second for
    either, it also better tests the limit of how many commands the
    communicator holds, with no hard limit being in the testing method, but
    only in the communicator class, adds at minimum 256 commands

--- Subcom15_PsudoSub.py
+++ Subcom15_PsudoSub.py
@@ -3,7 +3,7 @@
 15Subcom Capstone Project
 Communication Protocol Testing
 @author: team SubCom15 - Wyatt
-Created on Mon Feb 5 2024
+Created on Fri Mar 1 2024
 --------------------------
 """
 from Subcom15_Communicator import Communicator
@@ -11,62 +11,55 @@ import random
 import time
 from pytictoc import TicToc
 
-def testCommunicators(numCommunicators):
+def communicate(numCommunicators, Title):
     totalCommands = list( None for _ in range(numCommunicators*256))
     for comNum in range(numCommunicators):
-        communicator = Communicator(f"Test Communicator {comNum}",f"Command0")
-        while 1:
-            # Generate a random number of commands to add and remove
-            addCount = random.randint(5, 10)
-            removeCount = random.randint(1,5)
+        communicator = Communicator(f"{Title} {comNum}",f"Command0")
+        seed = 1
+        for seed in range (256):
+            random.seed(seed)
             currCount = len(communicator.commandList())
-
-            if currCount > 255:
-                print(f"Communicator {comNum} reached maximum commands: {len(communicator.commandList())}")
-                # Send all commands in random order
-                shuffledCommands = random.sample(communicator.commandList(), len(communicator.commandList()))
-                commandNum = 0
-                for sendCmd in shuffledCommands:
-                    communicator.sendCommand(sendCmd)
-                    time.sleep(1)
-                    receivedCmd = communicator.readCommand()
-                    while receivedCmd == '':
-                        time.sleep(0.01)
-                        receivedCmd = communicator.readCommand()
-                    totalCommands[comNum*256 + commandNum] = [sendCmd,receivedCmd]
-                    commandNum = commandNum + 1
-                    communicator.join()
-                break
-            else:
-                # Add commands
-                while len(communicator.commandList()) < currCount + addCount:
-                    if len(communicator.commandList()) <= 255:
-                        communicator.addCommand(f"Command{random.randint(0, 1024)}")
-                    else:
-                        break
-                
-                # Remove commands from random locations
-                removeCommands = random.sample(communicator.commandList(), removeCount)
-                for cmd in removeCommands:
-                    if len(communicator.commandList()) < 255:
-                        communicator.removeCommand(cmd)
-    return totalCommands
-                
             
+            # Add commands of Random values
+            addCount = random.randint(6, 10)
+            while len(communicator.commandList()) < currCount + addCount:
+                communicator.addCommand(f"Command{random.randint(0, 1024)}")
+            
+            # Remove commands from random locations
+            removeCommands = random.sample(communicator.commandList(), random.randint(1,5))
+            for cmd in removeCommands:
+                communicator.removeCommand(cmd)
+        
+        for i in range(1,comNum+1): seed = seed * i
+        random.seed(seed)
+        # Send all commands in random order
+        shuffledCommands = random.sample(communicator.commandList(), len(communicator.commandList()))
+        commandNum = 0
+        for sendCmd in shuffledCommands:
+            if(Title == "Daughter"):
+                receivedCmd = communicator.readCommand()
+                while receivedCmd == '':
+                    time.sleep(0.01)
+                    receivedCmd = communicator.readCommand()
+                communicator.sendCommand(sendCmd)
+                time.sleep(1)
+            elif(Title == "Master"):
+                communicator.sendCommand(sendCmd)
+                time.sleep(1)
+                receivedCmd = communicator.readCommand()
+                while receivedCmd == '':
+                    time.sleep(0.01)
+                    receivedCmd = communicator.readCommand()
+            totalCommands[comNum*256 + commandNum] = [sendCmd,receivedCmd]
+            commandNum = commandNum + 1
+            communicator.join()
+    return totalCommands
+        
 # Testing
-totalCommands = testCommunicators(1)
-print(f"Total number of commands: {len(totalCommands)}")
+testRuns = 1
+totalDaughterCommands = communicate(testRuns, "Daughter")
+totalMasterCommands = communicate(testRuns, "Master")
+print(f"Master's Total number of commands: {len(totalMasterCommands)}")
 print(f"Sent | Received")
-print(totalCommands)
-# communicator = Communicator(f"Test Communicator {0}",f"Command0")
+print(totalMasterCommands)
 
-
-# random_command = random.choice(communicator.commandList())
-# communicator.setSendCommand(random_command)
-# communicator.sendCommandEx()
-# time.sleep(1)   
-# read = communicator.readCommand()
-# time.sleep(1)
-# communicator.join()
-
-# Look into multiprocessing toolbox
