91cb45cd7197c257e7fdf3be81032cd3637857e4
Knight Scott <knightscott@gmail.com>
2024-03-09 14:14:42 -0700

    Demod bandpass set to ranges of 500 around frequencies

--- demod.m
+++ demod.m
@@ -11,11 +11,11 @@ function demod_sig = demod(bfsk, F1, F2, fs, bitLength)
     instantaneous_frequency = diff(instantaneous_phase) / (2 * pi * (1/fs));
 
     % Set a threshold to decide between the two frequencies
-    threshold = (F1 + F2) / 2;
+    threshold = ((F1-500) + (F2-500)) / 2;
 
     % Demodulation based on instantaneous frequency
     demod_sig = zeros(size(bfsk));
     demod_sig(instantaneous_frequency >= threshold) = 1;
-
+    disp(bitLength);
     demod_sig = demod_sig(round((fs/bitLength)/2):round(fs/bitLength):end-round((fs/bitLength)/2));
 end
