5146f017791c517e560ad42b6a17591126c6348c
Knight Scott <knightscott@gmail.com>
2024-04-05 15:55:22 -0600

    Daughter: 10 tests, Normal

--- Subcom15_Daughter.py
+++ Subcom15_Daughter.py
@@ -1,8 +1,8 @@
 from Subcom15_PsudoSub import communicate
 
-testRuns = 1
+testRuns = 10
 testType = 3
-randomType = 'R'
+randomType = 'N'
 totalDaughterCommands = communicate(testRuns, "Daughter", testType, randomType)
 print(f"Daughter's Total number of commands: {len(totalDaughterCommands)}")
 print(f"Sent | Received")
