4ade387c613f41b9f8e81cd190f0e5c0ae718976
Wyatt Weller <wyattzyatt@gmail.com>
2024-04-10 19:13:54 -0600

    Mo/Da: Removed Print Statements

--- Subcom15_Daughter.py
+++ Subcom15_Daughter.py
@@ -3,7 +3,5 @@ from Subcom15_PsudoSub import communicate
 testRuns = 10
 testType = 3
 randomType = 'N'
-totalDaughterCommands = communicate(testRuns, "Daughter", testType, randomType)
-print(f"Daughter's Total number of commands: {len(totalDaughterCommands)}")
-print(f"Sent | Received")
-print(totalDaughterCommands)
+communicate(testRuns, "Daughter", testType, randomType)
+print(f"Done")
--- Subcom15_Mother.py
+++ Subcom15_Mother.py
@@ -3,7 +3,5 @@ from Subcom15_PsudoSub import communicate
 testRuns = 10
 testType = 3
 randomType = 'N'
-totalMotherCommands = communicate(testRuns, "Mother", testType, randomType)
-print(f"Mother's Total number of commands: {len(totalMotherCommands)}")
-print(f"Sent | Received")
-print(totalMotherCommands)
+communicate(testRuns, "Mother", testType, randomType)
+print("Done")
